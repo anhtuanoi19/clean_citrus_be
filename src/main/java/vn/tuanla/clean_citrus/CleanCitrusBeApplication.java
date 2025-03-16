93e7256f7be02865c8d5e49e4b135084e45e838d
TuanLA <tuanla19161@gmail.com>
2025-03-16 23:37:38 +0700

    Fist commit

---
+++ src/main/java/vn/tuanla/clean_citrus/CleanCitrusBeApplication.java
@@ -0,0 +1,13 @@
+package vn.tuanla.clean_citrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCitrusBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanCitrusBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/tuanla/clean_citrus/CleanCitrusBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tuanla.clean_citrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCitrusBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
